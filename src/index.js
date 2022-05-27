d8e60aec34965cd55bbe3d19b857bc3076cc517d
Souru-Hishinuma <105961234+Souru-Hishinuma@users.noreply.github.com>
2022-05-27 23:50:23 +0900

    完成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストの内容取得
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  createIncompleteList(inputText);
+};
+// ターゲットを削除する関数
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+//　未完了リストに追加する処理
+const createIncompleteList = (text) => {
+  const li = document.createElement("li");
+
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const p = document.createElement("p");
+  p.innerText = text;
+
+  li.appendChild(div);
+  div.appendChild(p);
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // 完了ボタンを押したときの処理
+  completeButton.addEventListener("click", () => {
+    deleteFromIncompleteList(completeButton.parentNode.parentNode);
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerText = text;
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      //テキストの取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+    addTarget.appendChild(p);
+    addTarget.appendChild(backButton);
+    li.appendChild(addTarget);
+    document.getElementById("complete-list").appendChild(li);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // 削除ボタンを押したときの処理
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(deleteButton.parentNode.parentNode);
+  });
+
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  document.getElementById("incomplete-list").appendChild(li);
+};
+// 追加ボタンクリック時処理
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
